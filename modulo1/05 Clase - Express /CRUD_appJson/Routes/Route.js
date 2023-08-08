b7f5d690c6d327e45ff4f85ce2c5e6b8f26bbedf
jeanmunoz <jeanmunoz@mingeneros.gob.ar>
2023-08-08 20:40:16 -0300

    borro cosas

--- modulo1/05 Clase - Express /CRUD_appJson/Routes/Route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const fs = require('fs');
-const accountRoutes = require('./account.js')
-
-router.use(accountRoutes)
-module.exports = router;
