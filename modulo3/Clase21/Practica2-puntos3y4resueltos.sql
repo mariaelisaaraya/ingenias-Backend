bc5d3ed8908280b8f84eca8af6ce7949cb85a334
Jesica Gutierrez <jesica89@gmail.com>
2023-09-26 19:38:30 -0300

    ejercicios corregidos

--- modulo3/Clase21/Practica2-puntos3y4resueltos.sql
+++ modulo3/Clase21/Practica2-puntos3y4resueltos.sql
@@ -1,9 +1,18 @@
+/* -   3. Ejecuta otra consulta de selección similar a la primera, aplicando la siguiente condición
+        -   el campo SupplierID sea igual a 5 y el campo CategoryID sea igual a 4
+
+-   4. Ejecuta otra consulta de selección similar a la primera, aplicando la siguiente condición
+        -   el campo UnitsInStock tenga valores entre 25 y 40 unidades
+*/
 
 /* punto 3 */
 
-SELECT * FROM northwind.products  where ProductID = 5 ORDER BY ProductName;
-SELECT * FROM northwind.products  where CategoryID = 4 ORDER BY ProductName;
+SELECT * FROM northwind.products  where SupplierID = 5 and CategoryID = 4 ORDER BY ProductName;
 
-/* punto 4 */
+/* punto 4 opcion 1 */
 
 SELECT * FROM northwind.products  where UnitsInStock >= 25 and UnitsInStock <= 40 ORDER BY ProductName;
+
+/* punto 4 opcion 2 */
+
+SELECT * FROM northwind.products  where UnitsInStock between 25 and 40 ORDER BY ProductName;
