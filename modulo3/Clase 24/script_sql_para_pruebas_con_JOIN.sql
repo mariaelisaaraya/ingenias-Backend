6951ebe9d814d90a455de1e787ba104efb97440c
jemprojects <jemprojectsgit@gmail.com>
2023-09-28 21:05:13 -0300

    clase24

--- modulo3/Clase 24/script_sql_para_pruebas_con_JOIN.sql
+++ modulo3/Clase 24/script_sql_para_pruebas_con_JOIN.sql
@@ -17,7 +17,7 @@ CREATE TABLE TIPOSDETELEFONO (
 -- POBLAR LA TABLA TIPOS DE TELÉFONO CON REGISTROS PREDETERMINADOS
 INSERT INTO TIPOSDETELEFONO
 VALUES (null, 'Personal'),
-	   (null, 'Oficina'),
+	(null, 'Oficina'),
        (null, 'Móvil'),
        (null, 'Skype'),
        (null, 'WhatsApp'),
@@ -27,7 +27,7 @@ VALUES (null, 'Personal'),
 -- POBLAR LA TABLA CONTACTOS CON REGISTROS
 INSERT INTO CONTACTOS
 VALUES (null, 'Cameron Howe', 'cameron@mutiny.com', '11-4455-6699', 3),
-	   (null, 'Donna Clark', 'donna@mutiny.com', '11-4455-0000', 3),
+	(null, 'Donna Clark', 'donna@mutiny.com', '11-4455-0000', 3),
        (null, 'Gordon Clark', 'gordou@mutiny.com', '11-4110-0909', 2),
        (null, 'Joe McMillian', 'joe@mutiny.com', '11-4844-1001', 6),
        (null, 'John Bosworth', 'bos@mutiny.com', '11-4844-1000', null),
