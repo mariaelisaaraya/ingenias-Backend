57f9f5301cc95ce3162afbf314ff81f54d8664d8
jemprojects <jemprojectsgit@gmail.com>
2023-10-23 18:32:37 -0300

    add modificacions

--- modulo3/Clase 29/nodejs-express-Sequelize-mysql-Simple/routes/books.js
+++ modulo3/Clase 29/nodejs-express-Sequelize-mysql-Simple/routes/books.js
@@ -1,54 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const Book = require('../database/models/Book');
+module.exports = (sequelize, Sequelize, DataTypes) => {
+  const Book = sequelize.define(
+    "book", // Model name
+    {
+      // Model attributes
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      title: {
+        type: DataTypes.STRING
+      },
+      author: {
+        type: DataTypes.STRING
+      },
+      published: {
+        type: DataTypes.BOOLEAN
+      },
+    },
+    {
+      // Options
+      timestamps: true,
+      underscrored: true,
+    }
+  );
 
-// INDEX /api/books
-router.get('/', (req, res) => {
-    Book.findAll().then(books => {
-        res.json(books);
-    })
-})
-
-// CREATE
-router.post('/', (req, res) => {
-    Book.create({
-        title: req.body.title,
-        body: req.body.body
-    }).then(post => {
-        res.json(post);
-    })
-});
-
-// READ /api/books/:id
-router.get('/:id', (req, res) => {
-    Book.findByPk(req.params.id).then(book => {
-        res.json(book);
-    })
-});
-
-// UPDATE /api/books/:id
-router.patch('/:id', (req, res) => {
-    Book.update({
-        title: req.body.title,
-        body: req.body.body
-    }, {
-        where: {
-            id: req.params.id
-        }
-    }).then(result => {
-        res.json(result);
-    });
-});
-
-// DELETE /api/books/:id
-router.delete('/:id', (req, res) => {
-    Book.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(result => {
-        res.json(result);
-    })
-});
-
-module.exports = router;
+  return Book;
+};
